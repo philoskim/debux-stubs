88d06e4c9b3b3c8303b730a295cf6988baaabc17
philoskim <philos99@gmail.com>
2018-11-08 20:58:02 +0900

    v0.5.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject philoskim/debux-stubs "0.5.1"
+(defproject philoskim/debux-stubs "0.5.2"
   :description "Debux library for production"
   :url "https://github.com/philoskim/debux-stubs"
   :license {"Eclipse Public License"
