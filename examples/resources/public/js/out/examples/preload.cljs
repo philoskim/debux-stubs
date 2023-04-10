466b63612afabd118a944e4ffdee1894bbd27474
Philos Kim <philos99@gmail.com>
2023-04-11 00:54:03 +0900

    all unneeded files deleted

--- examples/resources/public/js/out/examples/preload.cljs
+++
@@ -1,6 +0,0 @@
-(ns examples.preload
-  (:require [debux.cs.core :as d]))
-
-;(d/set-debug-mode! false)
-;(d/set-ns-whitelist! ["example.clog*"])
-;(d/set-ns-blacklist! ["example.clogn"])
--- examples/resources/public/js/out/examples/preload.js
+++
@@ -1,6 +0,0 @@
-// Compiled by ClojureScript 1.10.238 {}
-goog.provide('examples.preload');
-goog.require('cljs.core');
-goog.require('debux.cs.core');
-
-//# sourceMappingURL=preload.js.map?rel=1628501140538
